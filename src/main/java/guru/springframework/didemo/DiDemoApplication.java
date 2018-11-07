b8072b12ed7ddb6b0df73ad13981b48fb6421ed3
AlibekMaks <Macsutov94@gmail.com>
2018-11-07 12:27:09 +0600

    Second push DiDemo

--- src/main/java/guru/springframework/didemo/DiDemoApplication.java
+++ src/main/java/guru/springframework/didemo/DiDemoApplication.java
@@ -1,6 +1,9 @@
 package guru.springframework.didemo;
 
+import guru.springframework.didemo.controller.ConstructorInjectedController;
 import guru.springframework.didemo.controller.MyController;
+import guru.springframework.didemo.controller.PropertyInjectedController;
+import guru.springframework.didemo.controller.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -9,8 +12,16 @@ import org.springframework.context.ApplicationContext;
 public class DiDemoApplication {
 
     public static void main(String[] args) {
-         ApplicationContext ctx =  SpringApplication.run(DiDemoApplication.class, args);
+        ApplicationContext ctx =  SpringApplication.run(DiDemoApplication.class, args);
         MyController myController = (MyController) ctx.getBean("myController");
         myController.hello();
+
+        // 3 строки сверху можно записать в одну строку
+//        SpringApplication.run(DiDemoApplication.class,args).getBean(MyController.class).hello();
+
+
+        System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
+        System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
+        System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
     }
 }
---
+++ src/main/java/guru/springframework/didemo/controller/SetterInjectedController.java
@@ -0,0 +1,26 @@
+package guru.springframework.didemo.controller;
+
+import guru.springframework.didemo.services.GreetingService;
+import guru.springframework.didemo.services.GreetingServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SetterInjectedController {
+
+    public  GreetingService greetingService;
+
+
+    public String sayHello(){
+        return greetingService.sayGreeting();
+    }
+
+
+    @Autowired
+    public void setGreetingService(@Qualifier("getterGreetingService") GreetingService greetingService){
+        this.greetingService=greetingService;
+    }
+
+
+}
